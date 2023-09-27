9c1b8814f1c009e12eb4ff54cf36198e330c7a02
Carter Frost <git@carterfrost.com>
2023-09-27 15:17:00 -0600

    arduino/2d_wheel_control/*.ino: use buffer instead to avoid left<->right

--- arduino/2d_wheel_control/2d_wheel_control.ino
+++ arduino/2d_wheel_control/2d_wheel_control.ino
@@ -20,7 +20,13 @@ byte rightEncoderB = 8;
 byte leftEncoderA = 0;
 byte leftEncoderB = 1;
 unsigned int serviced = 0;
-unsigned int serviced_limit = 10;
+unsigned int serviced_limit = 100;
+int speedL, speedR;
+
+
+const int bufferSize = 32; // expected max buffer "-100 -100\n" //so ~3x just incase
+char buffer[bufferSize];
+int bufferIndex = 0;
 
 Movement move = Movement(rightSpeedPin, rightDirectionA, rightDirectionB, leftSpeedPin, leftDirectionA, leftDirectionB);
 BasicEncoder leftEncoder(leftEncoderA,leftEncoderB);
@@ -46,15 +52,20 @@ void setup(){
 } //end setup
 
 void loop() {
-  if (Serial.available()) {
-    int speedL = Serial.parseInt();
-    int speedR = Serial.parseInt();
-    
-    if (speedL == 0 && speedR == 0) { move.stop(); }
-    if (speedL >= 0 && speedR >= 0) { move.forward(speedL, speedR); }
-    else if (speedL <= 0 && speedR <= 0) { move.backward(speedL*-1, speedR*-1); }
-    else if (speedL <= 0 && speedR >= 0) { move.rotateLeft(speedL*-1, speedR); }
-    else { move.rotateRight(speedL, speedR*-1); }
-  } //end Serial connected
-  
+  while (Serial.available()) {
+        char c = Serial.read();
+        if (c == '\n') { // Check for newline character
+            buffer[bufferIndex] = '\0';  // null-terminate the C string
+            if (sscanf(buffer, "%d %d", &speedL, &speedR) == 2) {
+              if (speedL == 0 && speedR == 0) { move.stop(); }
+              if (speedL >= 0 && speedR >= 0) { move.forward(speedL, speedR); }
+              else if (speedL <= 0 && speedR <= 0) { move.backward(speedL*-1, speedR*-1); }
+              else if (speedL <= 0 && speedR >= 0) { move.rotateLeft(speedL*-1, speedR); }
+              else { move.rotateRight(speedL, speedR*-1); }
+            } //emd got 2 ints
+            bufferIndex = 0;  // reset the buffer
+        } else if (bufferIndex < bufferSize - 1) {  // Ensure we don't overflow the buffer
+            buffer[bufferIndex++] = c;
+        } //end fill buffer
+    }//end Serial connected  
 } //end loop
