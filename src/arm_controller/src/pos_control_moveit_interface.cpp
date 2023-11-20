a5a1484660b827b3c9d6a770bc5471e2f09b91fb
Matthew Fricke <matthew@fricke.co.uk>
2023-11-20 15:45:27 -0700

    Added command pose subscriber Allow the node to receive target pose messages. Just commanding x for now.

--- src/arm_controller/src/pos_control_moveit_interface.cpp
+++ src/arm_controller/src/pos_control_moveit_interface.cpp
@@ -3,6 +3,9 @@
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
 
+// Find out what this is
+using std::placeholders::_1;
+
 // Run launch file: ros2 launch interbotix_xsarm_moveit xsarm_moveit.launch.py robot_model:=wx250 hardware_type:=fake
 
 // Planning execution rejected
@@ -77,14 +80,32 @@ class pos_control_moveit_interface : public rclcpp::Node
       // Manually set MoveIt parameters
       //this->declare_parameter("robot_description", "/home/calvinjs/MARIAM/install/interbotix_xsarm_descriptions/share/interbotix_xsarm_descriptions/urdf/px100.urdf.xacro");
       //this->declare_parameter("robot_description_semantic", "/home/calvinjs/MARIAM/install/interbotix_xsarm_moveit/share/interbotix_xsarm_moveit/config/srdf/px100.srdf.xacro");
-      
+
+      // 10 is some magic QoS number - find out what and give it a name
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      subscriber_ = this->create_subscription<geometry_msgs::msg::Pose>("/arm_cmd", 10, std::bind(&pos_control_moveit_interface::armPoseCmdSubscriberCallback, this, _1));
       timer_ = this->create_wall_timer(5s, std::bind(&pos_control_moveit_interface::timer_callback, this));
       //move_group_interface = new MoveGroupInterface(*this, "interbotix_arm");
     }
     //MoveGroupInterface *move_group_interface;
 
   private:
+
+  // member variables
+  double x_cmd = 0;
+  
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr subscriber_;
+  size_t count_;
+  
+  void armPoseCmdSubscriberCallback(const geometry_msgs::msg::Pose::SharedPtr msg)
+    {
+      x_cmd = msg->position.x;
+      RCLCPP_INFO(this->get_logger(), "Received: '%f'", x_cmd);
+    }
+    
+  
     void timer_callback()
     {
       auto message = std_msgs::msg::String();
@@ -93,16 +114,24 @@ class pos_control_moveit_interface : public rclcpp::Node
       publisher_->publish(message);
 
       auto move_group_interface = MoveGroupInterface(shared_from_this(), "interbotix_arm");
-
+      
       // Set a target Pose
-      auto const target_pose = []{
-        geometry_msgs::msg::Pose msg;
-        msg.orientation.w = 1.0;
-        msg.position.x = 0;
-        msg.position.y = 0;
-        msg.position.z = 0.23;
-        return msg;
-      }();
+      //auto const target_pose = []{
+      //  geometry_msgs::msg::Pose msg;
+      //  msg.orientation.w = 1.0;
+      //  msg.position.x = 0.2;
+      //  msg.position.y = 0;
+      //  msg.position.z = 0.23;
+      //  return msg;
+      //}();
+
+      geometry_msgs::msg::Pose target_pose;
+      target_pose.orientation.w = 1.0;
+      target_pose.position.x = x_cmd;
+      target_pose.position.y = 0;
+      target_pose.position.z = 0.23;
+      
+      
       move_group_interface.setPoseTarget(target_pose);
 
       // Create a plan to that target pose
@@ -120,9 +149,6 @@ class pos_control_moveit_interface : public rclcpp::Node
         RCLCPP_ERROR(this->get_logger(), "Planning failed!");
       }
     }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
 };
 
 int main(int argc, char * argv[])
