5795766b5697fc7ba1c242fddc1a395c9b3f9e10
cjstahoviak <cjstahoviak@gmail.com>
2023-11-21 11:51:14 -0700

    Initilize MoveGroupInterface as class variable

--- src/arm_controller/src/pos_control_moveit_interface.cpp
+++ src/arm_controller/src/pos_control_moveit_interface.cpp
@@ -1,72 +1,19 @@
+// CPP Headers
 #include <memory>
-
-#include <rclcpp/rclcpp.hpp>
-#include <moveit/move_group_interface/move_group_interface.h>
-
-// Find out what this is
-using std::placeholders::_1;
-
-// Run launch file: ros2 launch interbotix_xsarm_moveit xsarm_moveit.launch.py robot_model:=wx250 hardware_type:=fake
-
-// Planning execution rejected
-
-/*
-int main(int argc, char * argv[])
-{
-  // Initialize ROS and create the Node
-  rclcpp::init(argc, argv);
-  auto const node = std::make_shared<rclcpp::Node>(
-    "pos_control_moveit_interface",
-    rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
-  );
-
-  // Create a ROS logger
-  auto const logger = rclcpp::get_logger("pos_control_moveit_interface");
-  RCLCPP_INFO(logger, "Logger created");
-
-  // Create the MoveIt MoveGroup Interface
-  using moveit::planning_interface::MoveGroupInterface;
-  auto move_group_interface = MoveGroupInterface(node, "interbotix_arm");
-  RCLCPP_INFO(logger, "MoveGroupInterface created!");
-
-  // Set a target Pose
-  auto const target_pose = []{
-    geometry_msgs::msg::Pose msg;
-    msg.orientation.w = 1.0;
-    msg.position.x = 0;
-    msg.position.y = 0;
-    msg.position.z = 0.23;
-    return msg;
-  }();
-  move_group_interface.setPoseTarget(target_pose);
-
-  // Create a plan to that target pose
-  auto const [success, plan] = [&move_group_interface]{
-    moveit::planning_interface::MoveGroupInterface::Plan msg;
-    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-    return std::make_pair(ok, msg);
-  }();
-
-  // Execute the plan
-  if(success) {
-    move_group_interface.execute(plan);
-    RCLCPP_ERROR(logger, "Planning successful!");
-  } else {
-    RCLCPP_ERROR(logger, "Planning failed!");
-  }
-
-  // Shutdown ROS
-  //rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
-}
-*/
-
 #include <chrono>
 #include <functional>
 #include <string>
 
+// ROS Headers
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
 #include "std_msgs/msg/string.hpp"
+
+// Placeholders are un speicifed objects "_1" "_3" "_2" etc. that represent parameteres used by the function returned from bind
+// A synchronized callback with have multiple placeholders
+using std::placeholders::_1;
+
+// Run launch file: ros2 launch arm_controller xsarm_moveit.launch.py robot_model:=px100 robot_name:=monica_arm hardware_type:=actual
 
 using namespace std::chrono_literals;
 using moveit::planning_interface::MoveGroupInterface;
@@ -75,80 +22,88 @@ class pos_control_moveit_interface : public rclcpp::Node
 {
   public:
     pos_control_moveit_interface()
-    : Node("pos_control_moveit_interface"), count_(0)
+    : Node("pos_control_moveit_interface")
     {
       // Manually set MoveIt parameters
       //this->declare_parameter("robot_description", "/home/calvinjs/MARIAM/install/interbotix_xsarm_descriptions/share/interbotix_xsarm_descriptions/urdf/px100.urdf.xacro");
       //this->declare_parameter("robot_description_semantic", "/home/calvinjs/MARIAM/install/interbotix_xsarm_moveit/share/interbotix_xsarm_moveit/config/srdf/px100.srdf.xacro");
 
-      // 10 is some magic QoS number - find out what and give it a name
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-      subscriber_ = this->create_subscription<geometry_msgs::msg::Pose>("/arm_cmd", 10, std::bind(&pos_control_moveit_interface::armPoseCmdSubscriberCallback, this, _1));
+      // 10 is the depth of the message queue
+      subscriber_ = this->create_subscription<geometry_msgs::msg::Pose>("/arm_cmd", 10, std::bind(&pos_control_moveit_interface::arm_cmd_callback, this, _1));
       timer_ = this->create_wall_timer(5s, std::bind(&pos_control_moveit_interface::timer_callback, this));
-      //move_group_interface = new MoveGroupInterface(*this, "interbotix_arm");
     }
-    //MoveGroupInterface *move_group_interface;
+    ~pos_control_moveit_interface() 
+    {
+      delete move_group_interface;
+    }
 
   private:
 
   // member variables
-  double x_cmd = 0;
-  
+  MoveGroupInterface *move_group_interface = nullptr;
+  geometry_msgs::msg::Pose pose_cmd;
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr subscriber_;
-  size_t count_;
   
-  void armPoseCmdSubscriberCallback(const geometry_msgs::msg::Pose::SharedPtr msg)
-    {
-      x_cmd = msg->position.x;
-      RCLCPP_INFO(this->get_logger(), "Received: '%f'", x_cmd);
-    }
+  void arm_cmd_callback(const geometry_msgs::msg::Pose::SharedPtr msg)
+  {
+    // Create a MoveGroupInterface once
+    if (move_group_interface == nullptr) move_group_interface = new MoveGroupInterface(shared_from_this(), "interbotix_arm");
+
+    pose_cmd = *msg;
+    RCLCPP_INFO(this->get_logger(), "Received pose {x:'%f', y:'%f', z:'%f'}", msg->position.x, msg->position.y, msg->position.z);
+
+    //auto move_group_interface = MoveGroupInterface(shared_from_this(), "interbotix_arm");
     
-  
-    void timer_callback()
-    {
-      auto message = std_msgs::msg::String();
-      message.data = "MoveIt Interface Node Operating" + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
+    move_group_interface->setPoseTarget(*msg);
 
-      auto move_group_interface = MoveGroupInterface(shared_from_this(), "interbotix_arm");
-      
-      // Set a target Pose
-      //auto const target_pose = []{
-      //  geometry_msgs::msg::Pose msg;
-      //  msg.orientation.w = 1.0;
-      //  msg.position.x = 0.2;
-      //  msg.position.y = 0;
-      //  msg.position.z = 0.23;
-      //  return msg;
-      //}();
+    // Create a plan to that target pose
+    auto const [success, plan] = [this]{
+      moveit::planning_interface::MoveGroupInterface::Plan msg;
+      auto const ok = static_cast<bool>(move_group_interface->plan(msg));
+      return std::make_pair(ok, msg);
+    }();
 
-      geometry_msgs::msg::Pose target_pose;
-      target_pose.orientation.w = 1.0;
-      target_pose.position.x = x_cmd;
-      target_pose.position.y = 0;
-      target_pose.position.z = 0.23;
-      
-      
-      move_group_interface.setPoseTarget(target_pose);
-
-      // Create a plan to that target pose
-      auto const [success, plan] = [&move_group_interface]{
-        moveit::planning_interface::MoveGroupInterface::Plan msg;
-        auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-        return std::make_pair(ok, msg);
-      }();
-
-      // Execute the plan
-      if(success) {
-        move_group_interface.execute(plan);
-        RCLCPP_ERROR(this->get_logger(), "Planning successful!");
-      } else {
-        RCLCPP_ERROR(this->get_logger(), "Planning failed!");
-      }
+    // Execute the plan
+    if(success) {
+      move_group_interface->execute(plan);
+      RCLCPP_ERROR(this->get_logger(), "Planning successful!");
+    } else {
+      RCLCPP_ERROR(this->get_logger(), "Planning failed!");
     }
+  }
+  
+  void timer_callback()
+  {
+    // auto message = std_msgs::msg::String();
+    // message.data = "MoveIt Interface Node Operating";
+
+    // auto move_group_interface = MoveGroupInterface(shared_from_this(), "interbotix_arm");
+
+    // // geometry_msgs::msg::Pose target_pose;
+    // // target_pose.orientation.w = 1.0;
+    // // target_pose.position.x = x_cmd;
+    // // target_pose.position.y = 0;
+    // // target_pose.position.z = 0.23;
+    
+    // move_group_interface.setPoseTarget(pose_cmd);
+
+    // // Create a plan to that target pose
+    // auto const [success, plan] = [&move_group_interface]{
+    //   moveit::planning_interface::MoveGroupInterface::Plan msg;
+    //   auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+    //   return std::make_pair(ok, msg);
+    // }();
+
+    // // Execute the plan
+    // if(success) {
+    //   move_group_interface.execute(plan);
+    //   RCLCPP_ERROR(this->get_logger(), "Planning successful!");
+    // } else {
+    //   RCLCPP_ERROR(this->get_logger(), "Planning failed!");
+    // }
+  }
 };
 
 int main(int argc, char * argv[])
