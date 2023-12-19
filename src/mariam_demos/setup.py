f966386a6ac8e195ec53efbface7a699278d3251
Carter Frost <git@carterfrost.com>
2023-12-18 22:02:55 -0700

    src/mariam_demos/setup.py: +resources & Distance node

--- src/mariam_demos/setup.py
+++ src/mariam_demos/setup.py
@@ -1,19 +1,31 @@
 import os
 from glob import glob
 from setuptools import find_packages, setup
+import os
 
 package_name = 'mariam_demos'
+
+# Function to get all files in a directory
+def get_package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            # Correctly join the path and filename
+            paths.append(os.path.join(path, filename))
+    return paths
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
+        data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        ('share/' + package_name + '/msg', ['msg/Int16Stamped.msg']),
+        # Correctly specify the resource and launch files
+        (os.path.join('share', package_name, 'resource'), get_package_files('resource')),
+        (os.path.join('share', package_name, 'launch'), get_package_files('launch'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -33,7 +45,8 @@ setup(
             'offset = src.offset:main',
             'offsetMoveBase = src.offsetMoveBase:main',
             'overlay = src.overlay:main',
-            'SineMoveBase = src.SineMoveBase:main'
+            'SineMoveBase = src.SineMoveBase:main',
+            'Distance = src.distance:main'
         ],
     },
 )
