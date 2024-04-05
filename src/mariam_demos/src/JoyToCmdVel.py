070979a3920310546cac4df69113d0a0a2d4d245
Carter Frost <git@carterfrost.com>
2024-04-05 17:41:03 -0600

    src/mariam_demos/src/JoyToCmdVel.py: creation Joy->cmd_vel node
    Hardcodeded for Ross

---
+++ src/mariam_demos/src/JoyToCmdVel.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+
+#So 0.3 meter/s  would be the max linear
+class JoyToCmdVelNode(Node):
+    def __init__(self):
+        super().__init__('joy_to_cmd_vel_node')
+        self.subscription = self.create_subscription(
+            Joy,
+            '/joy',
+            self.joy_callback,
+            10)
+        self.publisher = self.create_publisher(Twist, '/Ross/cmd_vel', 10)
+
+    def joy_callback(self, msg):
+        twist = Twist()
+        twist.linear.x = msg.axes[1]/3
+        twist.angular.z = msg.axes[0]/3
+        self.publisher.publish(twist)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = JoyToCmdVelNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
