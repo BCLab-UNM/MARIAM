b911b83ba3a35a90daca5cb184d5a9007c6b3f7f
Carter Frost <git@carterfrost.com>
2024-04-08 17:38:28 -0600

    src/mariam_demos/src/JoyToCmdVel.py:+hostname cmd_vel, limit 0.125m/s

--- src/mariam_demos/src/JoyToCmdVel.py
+++ src/mariam_demos/src/JoyToCmdVel.py
@@ -1,24 +1,27 @@
+#!/usr/bin/python3
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
+import socket
 
 
 #So 0.3 meter/s  would be the max linear
 class JoyToCmdVelNode(Node):
     def __init__(self):
-        super().__init__('joy_to_cmd_vel_node')
+        hostname = socket.gethostname()
+        super().__init__(hostname+'_joy_to_cmd_vel_node')
         self.subscription = self.create_subscription(
             Joy,
             '/joy',
             self.joy_callback,
             10)
-        self.publisher = self.create_publisher(Twist, '/Ross/cmd_vel', 10)
+        self.publisher = self.create_publisher(Twist, '/'+hostname+'monica/cmd_vel', 10)
 
     def joy_callback(self, msg):
         twist = Twist()
-        twist.linear.x = msg.axes[1]/3
-        twist.angular.z = msg.axes[0]/3
+        twist.linear.x = msg.axes[1]/8
+        twist.angular.z = msg.axes[0]/8
         self.publisher.publish(twist)
 
 def main(args=None):
