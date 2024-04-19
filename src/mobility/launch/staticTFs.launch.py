daa8dd8730671eca136e6bca8bf88beacb2269e1
Carter Frost <git@carterfrost.com>
2024-04-19 14:46:29 -0600

    src/mobility/launch/staticTFs.launch.py: +top, flip mast

--- src/mobility/launch/staticTFs.launch.py
+++ src/mobility/launch/staticTFs.launch.py
@@ -9,10 +9,19 @@ def generate_launch_description():
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
-            name='base_link_to_mast_link',
+            name='mast_link_base_link',
             arguments=[
-                '-0.2032', '0.0', '0.4826', '0', '0', '0', '1',
-                f'{hostname}/base_link', f'{hostname}/mast_link'  
+                '0.2032', '0.0', '-0.4826', '0', '0', '0', '1',
+                f'{hostname}/mast_link', f'{hostname}/base_link'  
+            ],
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='mast_link_base_link',
+            arguments=[
+                '0.0', '0.0', '-0.09', '0', '0', '0', '1',
+                f'{hostname}/top_link', f'{hostname}/base_link'  
             ],
         ),
         Node(
