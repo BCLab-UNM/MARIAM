4b531f8a09a3b415dda2818cad4b7243f8adb09c
Carter Frost <git@carterfrost.com>
2024-04-22 15:36:59 -0600

    /home/carter/MARIAM/src/mariam_demos/*/*ceiling_camera*: +frame ids

--- src/mariam_demos/launch/start_ceiling_camera.launch.py
+++ src/mariam_demos/launch/start_ceiling_camera.launch.py
@@ -10,6 +10,7 @@ def generate_launch_description():
             parameters=[{
                 'camera_name': 'ceiling_camera',
                 'device': '/dev/video_ceiling',
+                'frame_id':  "ceiling_camera",
                 'video_device': '/dev/video4',
                 'image_width': 800,
                 'image_height': 448,
