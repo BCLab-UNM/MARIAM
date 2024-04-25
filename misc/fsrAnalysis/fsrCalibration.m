eaf701eea4f96cb9034d910effb6d05f6a1ec054
Calvin Stahoviak <cjstahoviak@gmail.com>
2024-04-24 19:10:39 -0600

    FSR calibration script is now generalized to work with the CSV and create figures

--- misc/fsrAnalysis/fsrCalibration.m
+++ misc/fsrAnalysis/fsrCalibration.m
@@ -2,78 +2,123 @@
 clear all
 close all
 
+% Check if the folder 'figures' exists, create it if it doesn't
+folderName = 'figures';
+if ~exist(folderName, 'dir')
+    mkdir(folderName);  % Make a new directory if it doesn't exist
+end
+
 % Read data from table
 % Specify the path to the CSV file
 filename = 'fsrCalibrationAverages1.csv';
 TData = readtable(filename);
 
 % Get data meta-data
-variableNames = TData.Properties.VariableNames(2:end);
-totalResistors = size(variableNames, 2);
-totalForces = size(variableNames, 1);
+resistorNames = TData.Properties.VariableNames(2:end);
+totalResistors = size(resistorNames, 2);
+totalForces = size(resistorNames, 1);
 colors = cool(totalResistors);
 
-% Observed data from 100kOhm pull down tests, averaged voltage sample TData
-% Converted from grams to newtons by dividing by 1000 and multiplying by 9.81
-% vOut_Jon =[0 1.236	2.062	2.958	3.802	4.254	4.644	4.838];
-% force_Jon = [0 0.04905	0.0981	0.1962	0.4905	0.981	1.962	4.905]; 
-
 % Assign voltageOut and Force data from file
-vOut = TData.x100k;
 voltageOut = TData(:, 2:end)
 force = (TData.Resistors / 1000)*9.81
 
-% Log-log scale plot we can see this relationship between voltage and force is nonlinear
+% Plot observed data
 legendTitles = {};
 figure
-for i = 1:length(variableNames)
-    varName = variableNames{i};
-    loglog(voltageOut.(varName), force, 'LineWidth', (0.5*i)+1, 'Color', colors(i, :));
-    legendTitles{end+1} = ['Force vs. Voltage ' strrep(varName, '_', '.')];
+for i = 1:length(resistorNames)
+    varName = resistorNames{i};
+    plot(voltageOut.(varName), force, 'LineWidth', 2, 'Color', colors(i, :));
+    legendTitles{end+1} = [strrep(varName(2:end), '_', '.') ' Ohm' ];
     hold on
 end
 hold off
 xlabel('Measured Voltage [V]')
 ylabel('Applied Force to Sensor [N]')
-legend(legendTitles, 'Location', 'northwest');  % Add a legend with descriptive labels
-title('Log-Log Scale Comparison of Voltage and Force');      % Add a title
+legend(legendTitles, 'Location', 'northwest');
+title('Observed Voltage vs. Force');
+set(gcf, 'Position', [100, 100, 800, 600]);
+saveas(gcf, fullfile('figures', 'ObservedVoltageVsForcePlot.png'));
+
+% Log-log scale plot we can see this relationship between voltage and force is nonlinear
+legendTitles = {};
+figure
+for i = 1:length(resistorNames)
+    varName = resistorNames{i};
+    loglog(voltageOut.(varName), force, 'LineWidth', 2, 'Color', colors(i, :));
+    legendTitles{end+1} = [strrep(varName(2:end), '_', '.') ' Ohm' ];
+    hold on
+end
+hold off
+xlabel('Measured Voltage [V]')
+ylabel('Applied Force to Sensor [N]')
+legend(legendTitles, 'Location', 'northwest');
+title('Log-Log Scale Comparison: Voltage vs. Force');
+axis([0 5 0 5])
+set(gcf, 'Position', [100, 100, 800, 600]);
+saveas(gcf, fullfile('figures', 'LogLogVoltageVsForcePlot.png'));
 
 % Take the exponent of the voltage 
-vOutExp = exp(vOut);
 expVoltageOut = exp(voltageOut)
+
+% Plot force against exponent of voltages for curve fitting
+legendTitles = {};
+figure
+for i = 1:length(resistorNames)
+    varName = resistorNames{i};
+    plot(expVoltageOut.(varName), force, 'LineWidth', 2, 'Color', colors(i, :));
+    legendTitles{end+1} = [strrep(varName(2:end), '_', '.') ' Ohm' ];
+    hold on
+end
+hold off
+xlabel('Exponential of Measured Voltage [V]')
+ylabel('Applied Force to Sensor [N]')
+legend(legendTitles, 'Location', 'northwest'); % Add a legend with descriptive labels
+title('Exponential Voltage vs. Force'); % Add a title
+set(gcf, 'Position', [100, 100, 800, 600]);
+saveas(gcf, fullfile('figures', 'ExponentialVoltageVsForcePlot.png'));
+
+% Determine coef for each resistor
+% Fit a 5th-degree polynomial and store coefficients
+coeffMatrix = zeros(length(resistorNames), 6);  % 6 coefficients for a 5th-degree polynomial
+for i = 1:length(resistorNames)
+    dataColumn = expVoltageOut{:, i};
+    coeff = polyfit(dataColumn, force, 5);
+    coeffMatrix(i, :) = coeff;
+end
+coeffTable = array2table(coeffMatrix', 'VariableNames', resistorNames)
 
 % With basic curve fitting we can get the coefficients of the nonlinear
 % Relationship with a polynomial with R=1 as:
-force_est = 1.60717263158763e-09*vOutExp.^5 ...
-    - 3.97326895848675e-07 * vOutExp.^4 ...
-    + 3.48267292566989e-05 * vOutExp.^3 ...
-    - 0.00117407617298212 * vOutExp.^2 ...
-    + 0.0234485875465875 * vOutExp	...
-    - 0.0225118029465446;
+for i = 1:totalResistors
+    forceEstimateArray(:, i) = ...
+       coeffTable{1, i} * expVoltageOut{:, i}.^5 ...
+     + coeffTable{2, i} * expVoltageOut{:, i}.^4 ...
+     + coeffTable{3, i} * expVoltageOut{:, i}.^3 ...
+     + coeffTable{4, i} * expVoltageOut{:, i}.^2 ...
+     + coeffTable{5, i} * expVoltageOut{:, i}	...
+     + coeffTable{6, i};
+end
+forceEstimate = array2table(forceEstimateArray, 'VariableNames', expVoltageOut.Properties.VariableNames)
 
-forceEstimate = 1.60717263158763e-09*expVoltageOut.^5 ...
-    - 3.97326895848675e-07 * expVoltageOut.^4 ...
-    + 3.48267292566989e-05 * expVoltageOut.^3 ...
-    - 0.00117407617298212 * expVoltageOut.^2 ...
-    + 0.0234485875465875 * expVoltageOut	...
-    - 0.0225118029465446
-
-% Plot the actual TData vs the estimated forces using the measured voltage
+% Plot the observed vs the estimated forces using the measured voltage
 figure
 legendTitles = {};
-for i = 1:length(variableNames)
-    varName = variableNames{i};
-    legendTitles{end+1} = ['Force vs. Voltage ' strrep(varName, '_', '.')];
+for i = 1 : length(resistorNames)
+    varName = resistorNames{i};
     
-    plot((voltageOut.(varName)),(force),'linewidth',3)
+    plot((voltageOut.(varName)),(force),'linewidth',1,'Color', colors(i, :));
+    legendTitles{end+1} = ['Observed ' strrep(varName(2:end), '_', '.') ' Ohm' ];
     hold on
-    plot(voltageOut.(varName),force_est,':','linewidth',3)
+    plot(voltageOut.(varName),forceEstimate{:, i},':','linewidth',3,'Color', colors(i, :));
+    legendTitles{end+1} = ['Estimated ' strrep(varName(2:end), '_', '.') ' Ohm' ];
     hold on    
-    %loglog(TData.(varName), force, 'LineWidth', (0.5*i)+1, 'Color', colors(i, :));
 end
 hold off
 xlabel('Measured Voltage [V]')
 ylabel('Force [N]')
-title('Comparison of TData and Estimated Forces');
-legend('TData','Estimated')
+title('Observed and Estimated Forces Compared');
+legend(legendTitles, 'Location', 'northwest');
 axis([0 5 0 5])
+set(gcf, 'Position', [100, 100, 800, 600]);
+saveas(gcf, fullfile('figures', 'ObservedvsEstimatedComparedVoltageVsForcePlot.png'));
