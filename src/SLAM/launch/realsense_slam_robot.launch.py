5ed7249d7d32c63c3a15d43a6b8c8c471cfd3d47
sob505 <sbarnaby505@unm.edu>
2024-05-24 13:07:33 -0600

    Updated realsense_slam_robot rtabmap_slam node to include namespace

--- src/SLAM/launch/realsense_slam_robot.launch.py
+++ src/SLAM/launch/realsense_slam_robot.launch.py
@@ -141,7 +141,8 @@ def generate_launch_description():
             package='rtabmap_slam', executable='rtabmap', output='screen',
             parameters=parameters,
             remappings=remappings,
-            arguments=['-d']),
+            arguments=['-d'],
+            namespace=host),
         
         # Because of this issue: https://github.com/IntelRealSense/realsense-ros/issues/2564
         # Generate point cloud from not aligned depth
