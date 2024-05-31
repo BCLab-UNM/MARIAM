ba7bfbc569fd7f31f1f53592464a25aae8ba0b94
Calvin Stahoviak <cjstahoviak@gmail.com>
2024-05-30 18:12:42 -0600

    chore: Update apply_rules.sh script to copy udev rules and reload them

--- script/apply_rules.sh
+++ script/apply_rules.sh
@@ -1,3 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-#TODO: Add a check to see if the rules are already applied
+# Directory containing udev rules
+RULES_DIR="./rules"
+
+# Check if the directory exists
+if [ ! -d "$RULES_DIR" ]; then
+  echo "Directory $RULES_DIR does not exist."
+  exit 1
+fi
+
+# Copy udev rules to /etc/udev/rules.d/
+for rule in "$RULES_DIR"/*.rules; 
+do
+  if [ -f "$rule" ]; then
+    sudo cp "$rule" /etc/udev/rules.d/
+    echo "Copied $rule to /etc/udev/rules.d/"
+  else
+    echo "No .rules files found in $RULES_DIR."
+    exit 1
+  fi
+done
+
+# Reload udev rules
+sudo udevadm control --reload-rules
+echo "Udev rules reloaded."
+
+# Trigger the new rules
+sudo udevadm trigger
+echo "Udev rules applied."
+
+echo "All udev rules in $RULES_DIR have been applied."
