45fe0e5bc7a016e8c269473e12f91d0e8032046e
Calvin Stahoviak <cjstahoviak@gmail.com>
2024-11-15 14:38:58 -0700

    Add note on disfunction

--- src/mariam_microros/launch/micro_ros_agent_launch.py
+++ src/mariam_microros/launch/micro_ros_agent_launch.py
@@ -3,6 +3,12 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+# Micro ROS currently does not support setting node or namespace names
+# on the client side, this has to be done in the .inu code.
+
+# Therefore this launch file will not currenlty do what its intended to
+# until an update is made to the micro_ros_agent package
+
 def generate_launch_description():
     # Declare a launch argument for robot_name
     robot_name_arg = DeclareLaunchArgument(
