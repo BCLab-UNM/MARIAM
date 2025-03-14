4921b1f307d85c5b0d51147129b86c9c9f36a88d
xavier <xbarr@unm.edu>
2025-03-13 20:42:50 -0600

    updated sine wave publisher

--- src/arm_controller/src/sine_wave_publisher.cpp
+++ src/arm_controller/src/sine_wave_publisher.cpp
@@ -1,12 +1,8 @@
 #include <memory>
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/pose.hpp"
 #include <cmath>
 #include "interbotix_xs_msgs/msg/joint_group_command.hpp"
-
-using namespace std::chrono_literals;
-using namespace geometry_msgs::msg;
 
 
 /**
@@ -27,41 +23,47 @@ class SineWavePublisher : public rclcpp::Node {
       msg.name = "arm";
       msg.cmd = {0, 0, 0, 0};
       publisher->publish(msg);
+    }
 
-      // start a timer to call "timer_callback"
-      timer = this->create_wall_timer(
-        (1 / frequency) * 1s,
-        std::bind(&SineWavePublisher::timer_callback, this)
-      );
+    void trajectory_trace() {
+      RCLCPP_INFO(LOGGER, "Starting trajectory trace");
+
+      const double AMP = M_PI_4;
+      const double FREQUENCY = 10; // in Hz
+
+      auto start_time = std::chrono::steady_clock::now();
+      auto end_time   = start_time + std::chrono::seconds(120);
+      double elapsed_time = 0.0;
+      
+      while(rclcpp::ok()) {
+        auto now = std::chrono::steady_clock::now();
+
+        if (now >= end_time) break;
+        
+        elapsed_time = std::chrono::duration<double>(now - start_time).count();
+
+        double cmd_value = AMP * std::sin((2.0 * M_PI / FREQUENCY) * elapsed_time);
+        auto msg = interbotix_xs_msgs::msg::JointGroupCommand();
+        msg.name = "arm";
+        msg.cmd = {0.0F, 0.0F, 0.0F, static_cast<float>(cmd_value)};
+        publisher->publish(msg);
+      }
+      RCLCPP_INFO(LOGGER, "Ending trajectory trace");
     }
 
 
   private:
-    int count = 0;
-    double frequency = 100; // in Hz
-    double complete_cycle = 6; // 3 seconds
-    double step = 2 * M_PI / (frequency * complete_cycle);
-
     rclcpp::TimerBase::SharedPtr timer;
     rclcpp::Publisher<interbotix_xs_msgs::msg::JointGroupCommand>::SharedPtr publisher;
     const rclcpp::Logger LOGGER = rclcpp::get_logger("sine_wave_publisher");
-
-    void timer_callback() {
-      float cmd = M_PI_4 * sin(step * count);
-      auto msg = interbotix_xs_msgs::msg::JointGroupCommand();
-      msg.name = "arm"; // specifies we want to move the arm
-      // creating the joint commands as a list
-      msg.cmd = {0, 0, 0, cmd};
-
-      publisher->publish(msg);
-      count++;
-    }
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<SineWavePublisher>());
+  auto node = SineWavePublisher();
+  std::this_thread::sleep_for (std::chrono::seconds(2));
+  node.trajectory_trace();
   rclcpp::shutdown();
   return 0;
 }
