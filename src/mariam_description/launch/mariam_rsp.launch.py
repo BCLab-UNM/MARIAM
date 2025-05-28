6ee2dc8f46259b5b713259f26589e5a538819581
Calvin Stahoviak <48141751+cjstahoviak@users.noreply.github.com>
2025-05-28 17:55:11 -0600

    EKF devel calvin (#75)
    * feat: imu launch file
    
    * feat: script to publish imu data only from rs camera
    
    * feat: robot state publisher for hardware
    
    * fix: realsense in namespace'd tf tree
    
    * chore: update realsense-ros submodule
    
    * chore: add submodule for multi-robot tools
    
    * Update realsense-ros submodule to use branch with tf_prefix support
    
    * feat: publish joint_states
    
    * ekf working
    
    * feat: experiments package created
    
    * feat: dynamic parameterization package created with node with boiler plate code
    
    * fix: update rviz
    
    * feat: EKF working and namespaces working
    
    * chore: remove realsense-ros subomdule
    
    * fix: correct launch file extension in setup.py

---
+++ src/mariam_description/launch/mariam_rsp.launch.py
@@ -0,0 +1,62 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from launch_ros.substitutions import FindPackageShare
+
+import xacro
+
+# Launch instructions:
+# ros2 launch mariam_description mariam_rsp.launch.py namespace:=robot1
+
+def generate_launch_description():
+    # Locate the package share directory.
+    pkg_share = FindPackageShare(package='mariam_description').find('mariam_description')
+    
+    # Define the robot model and rviz config file paths.
+    robot_name_in_urdf = 'mariam'
+    xacro_model_path = os.path.join(pkg_share, 'xacro_models/mariam.urdf.xacro')
+  
+    # Launch configuration variables.
+    model = LaunchConfiguration('model')
+    namespace = LaunchConfiguration('namespace')
+  
+    # Declare the launch arguments.
+    declare_model_path_cmd = DeclareLaunchArgument(
+        name='model', 
+        default_value=xacro_model_path, 
+        description='Absolute path to robot urdf file'
+    )
+
+    declare_namespace_cmd = DeclareLaunchArgument(
+        name='namespace',
+        default_value='',
+        description='Namespace for the robot'
+    )
+  
+  
+    # Robot state publisher node.
+    start_robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        namespace=namespace,
+        parameters=[{
+            'robot_description': Command(['xacro ', model])
+        }],
+        remappings=[
+            ('/tf', 'tf'),
+            ('/tf_static','tf_static'),
+            ('/joint_states', 'joint_states'),
+        ]
+    )
+  
+    # Create the launch description and add actions.
+    ld = LaunchDescription()
+  
+    ld.add_action(declare_model_path_cmd)
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(start_robot_state_publisher_cmd)
+
+    return ld
---
+++ src/mariam_vision/launch/realsense_imu.launch.py
@@ -0,0 +1,76 @@
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ament_index_python.packages import get_package_share_directory
+
+import os
+
+# Launch instructions:
+# ros2 launch mariam_vision realsense_imu.launch.py tf_prefix:=robot1 camera_namespace:=robot1
+
+
+def generate_launch_description():
+    # Launch arguments.
+    camera_namespace = LaunchConfiguration('camera_namespace')
+    tf_prefix = LaunchConfiguration('tf_prefix')
+    enable_gyro = LaunchConfiguration('enable_gyro')
+    enable_accel = LaunchConfiguration('enable_accel')
+
+    # Determine the path to the rs_launch.py file.
+    realsense2_package_share_directory = get_package_share_directory('mariam_vision')
+    rs_launch_file = os.path.join(realsense2_package_share_directory, 'launch', 'rs_launch_modified.py')
+
+    # Include rs_launch.py and pass the parameters.
+    rs_launch_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(rs_launch_file),
+        launch_arguments={
+            'camera_namespace': camera_namespace,
+            'tf_prefix': tf_prefix,
+            'enable_gyro': enable_gyro,
+            'enable_accel': enable_accel,
+        }.items()
+    )
+
+    return LaunchDescription([
+        # Declare launch arguments.
+        DeclareLaunchArgument(
+            'camera_namespace',
+            default_value='camera',
+            description='Namespace for the camera'
+        ),
+        DeclareLaunchArgument(
+            'tf_prefix',
+            default_value='',
+            description='Namespace for /tf and /tf_static'
+        ),
+        DeclareLaunchArgument(
+            'enable_gyro',
+            default_value='true',
+            description='Enable gyro stream (passed as enable_gyro to rs_launch.py)'
+        ),
+        DeclareLaunchArgument(
+            'enable_accel',
+            default_value='true',
+            description='Enable accel stream'
+        ),
+        DeclareLaunchArgument(
+            'unite_imu_method',
+            default_value='1',
+            description='[0-None, 1-copy, 2-linear_interpolation]'
+        ),
+        DeclareLaunchArgument(
+            'enable_depth',
+            default_value='false',
+            description='enable depth stream'
+        ),
+        DeclareLaunchArgument(
+            'enable_color',
+            default_value='false',
+            description='enable color stream'
+        ),
+        rs_launch_include
+    ])
