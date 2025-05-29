1517aed3b53f408e26f9a48fdf5a22196a21e57b
xavier <xbarr@unm.edu>
2025-05-29 12:43:34 -0600

    fix: fixed start_robot.launch.py

--- src/mariam_experiments/launch/start_robot.launch.py
+++ src/mariam_experiments/launch/start_robot.launch.py
@@ -12,119 +12,76 @@ from launch_ros.substitutions import FindPackageShare
 
 import os
 
-"""
-This file serves as a meta launch file for launching the appropriate things
-to perform experiments.
-
-It can be used to launch the experiment for the real robots or the simulator.
-
-For the real robots, this launch file must be run on each robot individually.
-"""
-
 
 def launch_setup(context, *args, **kwargs):
     robot_name_launch_arg = LaunchConfiguration('robot_name')
-    use_gazebo_launch_arg = LaunchConfiguration('use_gazebo')
     admittance_control_launch_arg = LaunchConfiguration('use_admittance_control')
 
-    # -----------------------------------------------------
-    # Stuff for the real robot
-    # -----------------------------------------------------
     px100_controller_desc = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
                 FindPackageShare('arm_controller'),
                 'launch',
                 'px100_controller.launch.py'
-            )
-        ),
+            ])
+        ]),
         launch_arguments={
             'robot_name': robot_name_launch_arg,
             'use_admittance_control': admittance_control_launch_arg,
             'use_fake_force': 'false',
             'use_sim': 'false'
-        },
-        condition=IfCondition(
-            PythonExpression([
-                "'", use_gazebo_launch_arg,
-                "' == 'false'"
-            ])
-        )
+        }.items()
     )
 
     micro_ros_desc = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
                 FindPackageShare('mariam_microros'),
                 'launch',
                 'micro_ros_agent_launch.py'
-            )
-        ),
-        condition=IfCondition(
-            PythonExpression([
-                "'", use_gazebo_launch_arg,
-                "' == 'false'"
             ])
-        )
+        ])
     )
 
     mariam_description_launch_desc = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
                 FindPackageShare('mariam_description'),
                 'launch',
                 'mariam_rsp.launch.py'
-            )
-        ),
+            ])
+        ]),
         launch_arguments={
             'namespace': robot_name_launch_arg,
             # TODO: does this need to be set???
             # 'use_sim_time': 'false',
-        },
-        condition=IfCondition(
-            PythonExpression([
-                "'", use_gazebo_launch_arg,
-                "' == 'false'"
-            ])
-        )
+        }.items()
     )
     
     realsense_imu_launch_desc = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
                 FindPackageShare('mariam_vision'),
                 'launch',
                 'realsense_imu.launch.py'
-            )
-        ),
+            ])
+        ]),
         launch_arguments={
             'namespace': robot_name_launch_arg,
-        },
-        condition=IfCondition(
-            PythonExpression([
-                "'", use_gazebo_launch_arg,
-                "' == 'false'"
-            ])
-        )
+        }.items()
     )
     
     hardware_ekf_launch_desc = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
                 FindPackageShare('mariam_navigation'),
                 'launch',
                 'hardware_ekf.launch.py'
-            )
-        ),
+            ])
+        ]),
         launch_arguments={
             'namespace': robot_name_launch_arg,
-        },
-        condition=IfCondition(
-            PythonExpression([
-                "'", use_gazebo_launch_arg,
-                "' == 'false'"
-            ])
-        )
+        }.items()
     )
 
     # TODO: add additional nodes for the real robot here
