9cc22bd938407224314a489f17a680a1af34b216
xavier <xbarr@unm.edu>
2025-05-28 18:59:23 -0600

    feat: started adding methods to execute each phase of the experiment to run_experiment.py

--- src/mariam_experiments/scripts/run_experiment.py
+++ src/mariam_experiments/scripts/run_experiment.py
@@ -1,33 +1,51 @@
+#!/usr/bin/env python3
+
 from fabric import SerialGroup
 import rclpy
 from rclpy.node import Node
 
+from std_msgs.msg import Float64
+from geographic_msgs.msg import Pose
+
+import time
 
 class ExperimentNode(Node):
 
     def __init__(self):
         super().__init__('experiment_node')
 
-        # a variable to keep track of the current stage of the experiment
-        self.experiment_stage = 'drive_up'
+        self.ross_arm_publisher = self.create_publisher(
+            Float64,
+            '/ross/px100_virtual_pose_updater',
+            10
+        )
+        
+        self.monica_arm_publisher = self.create_publisher(
+            Float64,
+            '/monica/px100_virtual_pose_updater',
+            10
+        )
 
         self.get_logger().info('Starting up the robots...')
         self.start_robots()
-
-    def control_loop(self):
-        """
-        This method is the main control loop for the experiment.
-        It will be used to monitor the state of each robot, and detect which
-        tasks need to be performed next.
-        """
-
-        try:
-            while rclpy.ok():
-                #                 
         
-        except KeyboardInterrupt:
-            self.get_logger().info('Shutting down...')
-        
+    # -----------------------------------------------------
+    # methods for each stage of the experiment
+    # -----------------------------------------------------
+    def drive_up(self):
+        pass
+
+    def lift_object(self):
+        height = 0.067
+        for i in range(100):
+            # increase the height by a millimeter
+            height += 0.001
+            self.ross_arm_publisher.publish(Float64(data=height))
+            self.monica_arm_publisher.publish(Float64(data=height))
+            time.sleep(45e-3)  # sleep for 45 milliseconds
+
+    def drive_away(self):
+        pass
 
     def start_robots(self):
         serial_group = SerialGroup(
@@ -43,3 +61,23 @@ class ExperimentNode(Node):
             self.get_logger().info(f'Robot {robot_name} started successfully.')
 
 
+def main(args=None):
+    rclpy.init(args=args)
+    experiment_node = ExperimentNode()
+
+    try:
+        # run the experiment stages
+        experiment_node.drive_up()
+        experiment_node.lift_object()
+        experiment_node.drive_away()
+    
+    except KeyboardInterrupt:
+        pass
+    
+    finally:
+        experiment_node.get_logger().info('Shutting down the experiment node...')
+        experiment_node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
