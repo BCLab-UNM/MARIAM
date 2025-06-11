713bc94a3032b3d5399c6242e02349485684d11a
xavier <xbarr@unm.edu>
2025-06-11 15:10:51 -0600

    feat: added the desired topics we want to track for experiments to run_experiment.sh

--- script/run_experiment.sh
+++ script/run_experiment.sh
@@ -2,7 +2,5 @@
 
 TIME=$(date +%m-%d-%Y__%H:%M:%S)
 
-# start up the ros2 bag
-# TODO: add the desired topics we want to listen to
-ros2 bag record /ross/commands/joint_group -o $PWD/src/mariam_experiments/bags/experiment_${TIME} &
+ros2 bag record /ross/tf /ross/force /ross/odometry/filtered /ross/amcl_pose /monica/tf /monica/force /monica/odometry/filtered /monica/amcl_pose -o $PWD/src/mariam_experiments/bags/experiment_${TIME} &
 ros2 run mariam_experiments run_experiment.py
