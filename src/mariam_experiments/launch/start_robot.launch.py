c1af6f4cfa77057b45c704f0879e1c8dd1fd9cfc
Calvin Stahoviak <cjstahoviak@gmail.com>
2025-08-07 14:05:40 -0600

    remove realsense launch

--- src/mariam_experiments/launch/start_robot.launch.py
+++ src/mariam_experiments/launch/start_robot.launch.py
@@ -141,8 +141,8 @@ def launch_setup(context, *args, **kwargs):
         px100_controller_desc,
         micro_ros_desc,
         mariam_description_launch_desc,
-        realsense_imu_launch_desc,
-        ekf_launch_desc,
+        # realsense_imu_launch_desc,
+        # ekf_launch_desc,
         # slam_launch_desc,
         # nav2_bringup_launch_desc
         # robot_follower_node
